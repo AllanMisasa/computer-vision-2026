a875b1d79fc773dd8743131390d623911f4b53d0
AllanMisasa <lesvoyagenostalgie@gmail.com>
2026-01-12 14:56:36 +0100

    Custom dataloader done

--- code/torchComponents/dataloading.py
+++ code/torchComponents/dataloading.py
@@ -1,6 +1,10 @@
 import  os
 import torch
+from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, TensorDataset
+import cv2
+import numpy as np
+from PIL import Image
 
 '''
 Dette er den lidt lettere måde at lave datasæt på end den indbyggede metode i PyTorch.
@@ -13,31 +17,21 @@ imagesClass2 = []
 labels = []
 
 # Hvilken mappe vores billededata ligger i
-rootDirClass1 = "../../images" 
-rootDirClass2 = ""
+trainDir = "../../images/pss/train/" 
+testDir = "../../images/pss/test/"
 
-'''
-Få relative stier til alle billeder og tilføj til images list.
-Selvom det er mere kompliceret en metode end glob eller path som ofte bliver foreslået til denne slags opgave
-så foretrækker jeg denne metode med kun det indbyggede os modul. Det er tydeligt hvad hele algoritmen gør,
-og vi kan få hele relative stier uden at sætte os ind i et moduls specifikke funktioner.
-Repeat for imagesClass2.
-'''
-for dirPath, subdirs, files in os.walk(rootDirClass1):
-    for file in files:
-        imagesClass1.append(os.path.join(dirPath, file))
+transform = transforms.Compose([
+            transforms.Resize(size=(64,64)),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.ToTensor()
+            ])
 
-print(imagesClass1)
+trainData = datasets.ImageFolder(root=trainDir,
+                               transform = transform,
+                               target_transform=None)
+print(f"Train data:\n{trainData}")
 
-'''
-Udfyld label listen så hvert billede også har et label.
-'''
-
-for i in range(len(imagesClass1)):
-    labels.append(1) 
-
-'''
-Tjek at vi har samme antal billeder og labels 
-'''
-print((len(imagesClass1)) == (len(labels)))
-
+testData = datasets.ImageFolder(root=testDir,
+                                transform = transform,
+                                target_transform=None)
+print(f"Test data:\n{testData}")
